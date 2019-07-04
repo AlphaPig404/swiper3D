8a005c6aa4917d82e4d1ca0ca02e6e757a85510a
fulun <fulun@mozat.com>
2019-07-04 12:05:44 +0800

    complete base version

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,18 @@
 import EventEmitter from 'eventemitter3';
-import { Options } from './type';
 import './css/index.scss';
+
+interface Options {
+    loop?: boolean,
+    start?: number,
+    showNum?: number
+}
 
 const defaultOpitons:Options = {
     showNum: 5,
     start: 0,
-    loops: false
+    loop: false
 }
+
 
 class Swiper3D extends EventEmitter{
     private options: Options
@@ -19,19 +25,18 @@ class Swiper3D extends EventEmitter{
 
     constructor(container: string|HTMLElement, options: Options){
         super()
-        this.options = {...options, ...defaultOpitons}
+        this.options = {...defaultOpitons, ...options}
         this.$swiper = typeof container === 'string' ? document.querySelector(container) : container 
       
         this.slides = this.$swiper.querySelectorAll('.swiper-slide')
         this.checkOptions(this.options)
-        
         this.init()
     }
 
     private init(){
         this.currentIndex = this.options.start
         this.middleIndex = Math.floor(this.slides.length / 2)
-        console.log(this.middleIndex)
+
         this.forEach(this.slides, (slide: HTMLElement, index) => {
             const SCALE_BASE = 0.8
             const ROTATE = 60
@@ -41,7 +46,7 @@ class Swiper3D extends EventEmitter{
             const absShowIndex = Math.abs(showIndex)
 
             const translateX: number = (1 - 1/Math.pow(2, absShowIndex))*sign
-            const rotateY: number = showIndex ? -sign*ROTATE : 0
+            const rotateY: number = showIndex ? - sign*ROTATE : 0
             const scale:number = Math.pow(SCALE_BASE, absShowIndex)
 
             const zIndex: number = showIndex === 0 ? 0 : - absShowIndex
@@ -54,27 +59,41 @@ class Swiper3D extends EventEmitter{
             this.styleList.push(cssText)
 
             slide.style.cssText = cssText
-            slide.dataset.index = "" + index
+            slide.dataset.index = "" + showIndex
             slide.addEventListener('click', (event:MouseEvent)=>{
                 const target = <HTMLButtonElement>event.target
                 this.jump(+target.dataset.index)
             })
         })
+        if(this.options.loop){
+            setInterval(this.next.bind(this), 1000)
+        }
     }
 
     public prev() {
-        
+        this.currentIndex -= 1
+        this.updateView()
     }
 
     public next(){
-
+        this.currentIndex += 1
+        this.updateView()
     }
 
     public jump(index: number){
-        console.log(index)
-        this.forEach(this.slides, (slide, i)=>{
-            const newIndex:number = (i + this.middleIndex - index) % this.slides.length
-            slide.style.cssText = this.styleList[newIndex]
+        this.currentIndex = index
+        this.updateView()
+    }
+
+    private updateView(){
+        this.forEach(this.slides, (slide, index) => {
+            let _index =  (index - this.currentIndex ) % this.slides.length
+
+            if(_index < 0) {
+                _index += this.slides.length
+            }
+
+            slide.style.cssText = this.styleList[_index]
         })
     }
 
